5cad607ae302f9c14eb9ffabe787bddcc20b3987
dc <divyanshc21@iitk.ac.in>
2024-04-04 20:59:21 +0530

    restructured

---
+++ milestone2/misc/sampleTest/test5.py
@@ -0,0 +1,24 @@
+class rect:
+
+    def __init__(self, length: int, width: int):
+        self.length:int = length
+        self.width:int = width
+        self.area:int = -1
+        self.perimeter:int = -1
+
+    def find_area(self) -> int:
+        self.area = self.length * self.width
+        return self.area
+
+    def find_perimeter(self) -> int:
+        self.perimeter = 2 * (self.length + self.width)
+        return self.perimeter
+
+def main():
+    obj1: rect = rect(3, 4)
+    print(obj1.find_area())
+    print(obj1.find_perimeter())
+    perimeter: int = obj1.find_perimeter()
+
+if __name__ == "__main__":
+  main()
