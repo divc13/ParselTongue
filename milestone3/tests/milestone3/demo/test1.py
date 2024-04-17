5ab4586b4de6e94ecf635857511e4db8ca3aed09
dc <divyanshc21@iitk.ac.in>
2024-04-17 06:01:46 +0530

    test modified

--- milestone3/tests/milestone3/demo/test1.py
+++ milestone3/tests/milestone3/demo/test1.py
@@ -14,16 +14,65 @@ def factorial(n:int) -> int:
 
     return ans
 
-def main():
-    n:int = 5
-    ans:int = factorial(n)
+def power(base: int, exp: int) -> int:
+    temp: int
+    ans:int
 
-    if ans > 0:
-        print(ans)
+    if exp == 0:
+        ans = 1
+    
+    elif exp == 1:
+        ans = base
+    
+    elif exp % 2 == 0:
+        temp = power(base, exp // 2)
+        ans = temp * temp
+    
+    else:
+        temp = power(base, exp // 2)
+        ans = base * temp * temp
+    
+    return ans
+    
+
+def sum_of_powers(n: int, exp: int) -> int:
+    tot_sum: int = 0
+
+    i: int = 200
+    for i in range(1, n + 1):
+        tot_sum += power(i, exp)
+
+    
+    return tot_sum
+
+
+
+def display_power_table(n:int, exp:int) -> None:
+
+    i:int
+    for i in range(1, n+1):
+        print(power(i, exp))
+
+
+def main():
+    print("abcd")
+
+    n:int = 3
+    ans1:int = sum_of_powers(n, 2);
+
+    print(ans1)
+
+    ans2: int = sum_of_powers(n, 3)
+
+    print(ans2)
+
+    ans3:int = factorial(n)
+    print(ans3)
+    
+    x:int = 10
+    display_power_table(x, 2)
+
 
 if __name__ == "__main__":
     main()
-
-
-    
-
+  
